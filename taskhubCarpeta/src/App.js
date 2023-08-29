06fb94eecabc398bdf4a8237e4893071f1a44372
julimejia <davidmejiao43@gmail.com>
2023-08-29 12:04:02 -0500

    base para programar

---
+++ taskhubCarpeta/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="text-gray-500 underline">
+      hola mundo funciona plz
+      ya sabemos que  deberia funcionar 
+    </div>
+  );
+}
+
+export default App;
